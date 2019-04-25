63f556de22a208c9f0f0666f93e50f0b9d6ae404
María Paulina Ramírez Vásquez <maria.ramirez@zinobe.com>
2019-04-25 15:02:55 -0500

    Ajuste para trabajar con peticiones POST

--- handler.js
+++ handler.js
@@ -1,11 +1,26 @@
 'use strict';
 
+const queryString = require('querystring');
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: `Hola ${event.pathParameters.name}`,
       input: event.name,
+    }, null, 2),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.showUser = async (event) => {
+  const body = queryString.parse(event['body']);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Petición POST',
+      input: `Hola ${ body.name } ${ body.lastname }`,
     }, null, 2),
   };
 
